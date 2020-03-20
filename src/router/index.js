33221ab48da94a01e1af79c37310744448adef91
Faisal Nassarawa <wearegrumie@gmail.com>
2020-03-20 14:03:56 +0100

    new

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/auth/Login.vue'
+import Dashboard from '../views/patients/Dashboard.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +19,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/patient-dashboard',
+    component: Dashboard
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/auth/SignupCustomer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div >
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  },
+  data(){
+
+  },
+  beforeMount(){
+
+  },
+  methods:{
+
+  },
+  created(){
+
+  }
+}
+</script>

733243981dffa2adff3ec35f3076c9d0f5d5e9e0
Grumie <wearegrumie@gmail.com>
2021-07-30 10:14:05 +0100

    new

--- src/views/tpas/ListTPAs.vue
+++ src/views/tpas/ListTPAs.vue
@@ -209,7 +209,7 @@ export default {
   },
   beforeMount(){
     this.user = JSON.parse(localStorage.getItem('user'))
-    this.axios.get(`/api/v1/auth/org_agency/${this.user.id}`)
+    this.axios.get(`/api/v1/auth/org_agency/95930`)
                 .then(response => {
                     this.tpas = response.data.data
                     console.log(response)
@@ -288,7 +288,7 @@ export default {
     getTPAs(){
       this.user = JSON.parse(localStorage.getItem('user'))
 
-      this.axios.get(`/api/v1/auth/org_agency/${this.user.id}`)
+      this.axios.get(`/api/v1/auth/org_agency/95930`)
                   .then(response => {
                       this.tpas = response.data.data
                       console.log(response)

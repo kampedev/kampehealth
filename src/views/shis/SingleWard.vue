762171695570e9f292a8447586faeaf887c77094
Grumie <wearegrumie@gmail.com>
2021-08-04 08:49:09 +0100

    ward page sector batch update

--- src/views/shis/SingleWard.vue
+++ src/views/shis/SingleWard.vue
@@ -21,15 +21,37 @@
            <div class="container">
 
                <div class="row list">
-                   <div class="col-lg-12 text-center">
+                   <div class="col-lg-12">
                        <div class="card m-b-30">
-                         <div class="card-header">
-                           <h4 class="h4">{{clients.length}} Ward Enrollees</h4>
+                         <div class="card-header text-center">
+                           <h4 class="h4">{{clients.data.length}} Ward Enrollees</h4>
                          </div>
 
                            <div class="card-body">
+                             <div class="col-md-12">
+                               <p class="h5">Basic Healthcare Provision Fund: {{clients.bhcpf}}</p>
+                               <p class="h5">State Equity Program: {{clients.sep}}</p>
 
+                             </div>
+                             <button class="btn btn-outline-primary" @click="show = !show">Bulk Update Sector</button>
+                             <div class="form-group col-md-8" v-show="show">
+                               <label >Select Sector </label>
 
+                               <select class="form-control"  v-model="sector" >
+                                   <option  value="State Civil Servant Healthcare Plan">State Civil Servant Healthcare Plan</option>
+                                   <option  value="Basic Healthcare Provision Fund">Basic Healthcare Provision Fund</option>
+                                   <option  value="State Equity Program">State Equity Program</option>
+                                   <option  value="Universal Basic Education Healthcare Plan">Universal Basic Education  Healthcare Plan</option>
+                                  <option  value="Voluntary Contributors Healthcare Plan">Voluntary Contributors Healthcare Plan</option>
+                                  <option  value="Voluntary Contributor">Voluntary Contributor</option>
+                                  <option  value="Organized Community Healthcare Plan">Organized Community Healthcare Plan</option>
+                                  <option  value="Oganized Private Sector Healthcare Plan">Oganized Private Sector Healthcare Plan</option>
+                              </select>
+
+                                <button type="button" class="btn btn-primary" @click="updateSector" style="margin-top:10px;">Update</button>
+
+                             </div>
+                             <!-- {{checked}} -->
 
 
                            </div>
@@ -48,6 +70,7 @@
                                  <table class="table align-td-middle table-card">
                                      <thead>
                                      <tr>
+                                         <th v-show="show">Marker</th>
                                          <th>Name</th>
                                          <th>Contact</th>
                                          <th>Status</th>
@@ -55,8 +78,16 @@
                                      </tr>
                                      </thead>
                                      <tbody>
-                                     <tr v-for="enrollee in clients" v-bind:key="enrollee.id">
+                                     <tr v-for="enrollee in clients.data" v-bind:key="enrollee.id">
 
+                                          <td v-show="show">
+                                            <div class="row form-check">
+                                              <input class="form-check-input" v-model="checked"
+                                               type="checkbox" :value="enrollee.id"
+                                               :id="enrollee.id" >
+
+                                             </div>
+                                          </td>
                                          <td>
                                            {{enrollee.firstname}} {{enrollee.lastname}}
 
@@ -65,9 +96,13 @@
                                          <td>{{enrollee.sector}}</td>
 
                                          <td>
+
+
+
                                            <router-link :to="{ path: '/client/'+ enrollee.id}">
-                                             <button type="button" name="button" class="btn btn-info">view</button>
+                                             <button type="button" name="button" class="btn btn-info"><i class="fe fe-eye"></i></button>
                                             </router-link>
+
                                          </td>
                                      </tr>
 
@@ -121,6 +156,9 @@ export default {
       user:null,
       clients:"",
       edit:false,
+      show:false,
+      sector:"",
+      checked: [],
       isLoading: false,
       fullPage: true,
       agency_id:""
@@ -130,19 +168,51 @@ export default {
     this.user = JSON.parse(localStorage.getItem('user'))
     this.axios.get(`/api/v1/auth/getuserbyward/${this.$route.params.id}`)
                 .then(response => {
-                    this.clients = response.data.data
+                    this.clients = response.data
                     console.log(response)
                 })
                 .catch(error => {
                     console.error(error);
                 })
   },
+  computed:{
+    //
+  },
   methods:{
+
+    updateSector(){
+    if (this.checked.length < 1 || this.sector == '') {
+      this.$toasted.error('No enrolleee or sector selected', {position: 'top-center', duration:3000 })
+
+    }
+    else {
+      this.isLoading = true
+        this.axios.patch(`/api/v1/auth/change-sector-bulk`,{
+          sector: this.sector,
+          id_list: this.checked,
+        })
+                    .then(response => {
+                        console.log(response)
+                        this.getUsers()
+                        this.$toasted.success('Enrollees Changed Successfully', {position: 'top-center', duration:3000 })
+                        this.isLoading = false
+                        this.show = false
+
+                    })
+                    .catch(error => {
+                        console.error(error);
+                        this.$toasted.error('Error!', {position: 'top-center', duration:3000 })
+                        this.isLoading = false
+
+                    })
+    }
+
+  },
     getUsers(){
       this.user = JSON.parse(localStorage.getItem('user'))
       this.axios.get(`/api/v1/auth/getuserbyward/${this.$route.params.id}`)
                   .then(response => {
-                      this.clients = response.data.data
+                      this.clients = response.data
                       console.log(response)
                   })
                   .catch(error => {

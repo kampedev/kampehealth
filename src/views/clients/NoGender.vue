a4a781ad3ce7bef09d63c7d9ad1e040d6f972cec
Grumie <wearegrumie@gmail.com>
2021-11-20 11:50:30 +0100

    filter end date corrected

--- src/views/clients/NoGender.vue
+++ src/views/clients/NoGender.vue
@@ -3,12 +3,17 @@
     <div class="col-lg-12 col-md-12">
       <div class="card m-b-30">
         <div class="card card-header">
-          <strong>Total  {{ clients.total_dep + clients.total_prin}} No Dates </strong>
-          <p>Dependents: {{clients.total_dep}} </p>
-          <p>Principal: {{clients.total_prin}} </p>
+          <p class="h5">Total to be Uploaded sheet: {{idcard_offline.length}} </p>
+          <p class="h5"> {{this.json_index}} Index Uploaded </p>
           <p>
             <button class="btn btn-primary" @click="autoUpdate">Update All</button>
           </p>
+
+          <div class="col-md-12"> 
+            <label for="">Start From Index: {{json_index}} </label>
+            <input type="text" class="form-control" v-model="json_index">
+
+          </div>
           
          
           <!-- <button class="btn btn-primary" @click="getlist">ffdd</button> -->
@@ -42,14 +47,7 @@
               </thead>
               <tbody>
                 <tr v-for="(client, index) in clients.data" v-bind:key="client.id">
-                  <!-- <td>
-                    <img
-                      :src="`https://api.hayokinsurance.com/image/${client.user_image}`"
-                      height="170px"
-                      width="120px"
-                      alt="User Photo"
-                    />
-                  </td> -->
+                 
                   <td>
                     <router-link
                       :to="{ path: '/client/' + client.id, params: {} }"
@@ -122,7 +120,7 @@ export default {
       gender: "",
       value_list: "",
       auto_selected: "",
-      newarray: [],
+      json_index: 0,
       idcard_offline: idcardJson,
       json_fields: {
         "First Name": "firstname",
@@ -229,49 +227,40 @@ export default {
 
  updateDateauto(){
       this.isLoading = true
-      //  this.auto_selected = this.clients.data[Math.random()]
-       this.auto_selected = this.clients.data[0]
-       console.log(this.auto_selected)
-       let formatter =  this.idcard_offline.filter((x) => x.id_card_number == this.auto_selected.id_card_number);
 
-     if (formatter.length >=1) {
-      this.user = JSON.parse(localStorage.getItem('user'))
-      this.axios.post(`/api/v1/auth/update-dates-all`,{
-        values: formatter
-      })
-            .then(response => {
-                
-                console.log(response)
-                this.getClients()
-                    this.isLoading = false
-                  this.$toasted.success('updated Successfully!', {position: 'top-center', duration:3000 })
+      // this.auto_selected = this.clients.data[0]
+      //  console.log(this.auto_selected)
+      //  let formatter =  this.idcard_offline.filter((x) => x.id_card_number == this.auto_selected.id_card_number);
 
-            })
-            .catch(error => {
-                console.error(error);
-                  this.isLoading = false
-                  this.$toasted.success('error!', {position: 'top-center', duration:3000 })
-            })
-     } else {
-     let  formatter = [
-          {
-             "dob": this.clients.data[0].dob,
-              "expiry_date": "01-01-2030",
-              "id_card_number": this.clients.data[0].id_card_number,
-              "category": this.clients.data[0].account_type
-           },
-       ]
+   
 
        this.user = JSON.parse(localStorage.getItem('user'))
-      this.axios.post(`/api/v1/auth/update-dates-all`,{
-        values: formatter
-      })
+      this.axios.post(`/api/v1/auth/multiverse/add`,{
+             "firstname": this.idcard_offline[this.json_index].firstname,
+             "lastname": this.idcard_offline[this.json_index].lastname,
+             "middlename": this.idcard_offline[this.json_index].middlename,
+             "gender": this.idcard_offline[this.json_index].gender,
+             "dob": this.idcard_offline[this.json_index].dob,
+             "expiry_date": this.idcard_offline[this.json_index].expiry_date,
+             "id_card_number": this.idcard_offline[this.json_index].id_card_number,
+             "CATEGORY": this.idcard_offline[this.json_index].CATEGORY,
+             "provider_id": this.idcard_offline[this.json_index].provider_id,
+             "place_of_work": this.idcard_offline[this.json_index].place_of_work,
+             "org_id": this.idcard_offline[this.json_index].org_id,
+             "user_image": this.idcard_offline[this.json_index].user_image,
+             "plan_type": this.idcard_offline[this.json_index].plan_type,
+             "nimc_number": this.idcard_offline[this.json_index].nimc_number,
+             "enrolled_by": this.idcard_offline[this.json_index].enrolled_by
+            
+     
+     })
             .then(response => {
                 
                 console.log(response)
-                this.getClients()
+                // this.getClients()
                     this.isLoading = false
-                  this.$toasted.success('updated!', {position: 'top-center', duration:3000 })
+                    this.json_index ++
+                  this.$toasted.success('updated!', {position: 'top-center', duration:1000 })
 
             })
             .catch(error => {
@@ -280,7 +269,6 @@ export default {
                   this.$toasted.success('error!', {position: 'top-center', duration:3000 })
             })
 
-     }
     },
 
     updateDate(client){

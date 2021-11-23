4f12365b3d6f7bf758fc1b0b464419f9ee6e9714
Grumie <wearegrumie@gmail.com>
2021-11-23 13:14:33 +0100

    test

--- src/main.js
+++ src/main.js
@@ -74,8 +74,8 @@ Vue.use(VueFullscreen)
 
 
 window.axios=axios
-axios.defaults.baseURL = 'https://api.hayokinsurance.com';
-// axios.defaults.baseURL = 'http://localhost:8000';
+// axios.defaults.baseURL = 'https://api.hayokinsurance.com';
+axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.headers.common['Content-Type'] = 'application/json'
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('jwt')
 // axios.defaults.headers.common['Access-Control-Allow-Origin'] = ''

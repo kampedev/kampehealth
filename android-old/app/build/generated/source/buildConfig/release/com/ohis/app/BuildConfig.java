db259401d926eee2d05f6eaae7d196331c2df6e2
Grumie <wearegrumie@gmail.com>
2023-01-02 23:53:13 +0100

    last comm

---
+++ android-old/app/build/generated/source/buildConfig/release/com/ohis/app/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ohis.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.oshia.app";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 8;
+  public static final String VERSION_NAME = "8.0";
+}
